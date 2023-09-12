d14959dc848120182667fa0b3a6fe49d79c536f2
Connor <99085769+TheDevConnor@users.noreply.github.com>
2023-09-11 21:53:09 -0400

    updated some things

--- script.js
+++ script.js
@@ -10,23 +10,56 @@ input.addEventListener('keydown', function (event) {
     }
 });
 
+function checkProject(projectName) {
+    switch (projectName) {
+        case 'zura': {
+        return `<img src="images/zura.png" alt="Zura Logo" width="100" height="100">
+    Zura is a custom programming language that I created. 
+    It is a dynamically typed language and is a high level language. Which I plane to turn
+    into a low level language. It is currently in development and is 
+    not ready for use. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Zura-v2">here</a>.`;
+        }
+        case 'chess': {
+return `Chess is a chess engine that I made using python. It is not that great of a 
+    chess engine, but it is a chess engine. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Basic-Python-Chess-Engine">here</a>.`;
+        }
+        case 'wordle': {
+return `Wordle is a word game that I made using html, css, and javascript. It is a game where 
+you have to guess a word in 6 tries. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Wordle-Clone">here</a>, and the video <a target="_blank" href="https://www.youtube.com/watch?v=savPJTlsv2k&t=160s">here</a>.`;
+        }
+    }
+}
+
 function executeCommand(command) {
     if (command === 'help') {
-        return 'Available commands: help, repo, project';
+        return 'Available commands: help, repo, project, clear';
     } else if (command === 'repo') {
-        return `Repository Information:
+        return `
+    Repository Information:
     - Repository Name: Terminal Website
     - Author: TheDevConnor
     - Description: A terminal style portfolio website
-    - URL: [Link to Repo](https://github.com/TheDevConnor/TerminalWebsite)`;
+    - URL: <a target="_blank" href="https://github.com/TheDevConnor/TerminalWebsite">Link to Repo</a>`;    
     } else if (command.startsWith('project')) {
         const projectArgs = command.split(' ');
         if (projectArgs.length === 2) {
             const projectName = projectArgs[1];
-            return `Project Information for ${projectName}: (Insert details here)`;
+            return `Project Information for ${projectName}: 
+            ${checkProject(projectName)}`;
         } else {
-            return 'Usage: project <project name>';
+            return "Please provide a project name from the list: zura, chess, wordle";
         }
+    } else if (command === 'clear') {
+        document.querySelector('.output').innerHTML = '';
+        return '';
+    } else if (command === 'about') {
+        return `
+I am a 18 year old programmer in his first year of collage. I have been programming 
+for 4 years now. I have made many projects in many different languages. I have 
+made a chess engine in python, a wordle clone in html, css, and javascript, and a 
+custom programming language in c++. I am currently working on a custom programming 
+language in c++ called Zura. I also have a youtube channel where I post 
+videos about programming. You can find my channel <a target="_blank" href="https://www.youtube.com/channel/UCpEYiOD5VxkK3iK7JmtbrPQ">here</a>.`;
     }
     return `Unknown command: ${command}`;
 }
@@ -34,11 +67,7 @@ function executeCommand(command) {
 function appendResponse(response) {
     const output = document.querySelector('.output');
     const responseText = document.createElement('pre');
-    responseText.textContent = response;
-    output.appendChild(responseText);
+    responseText.innerHTML = response; // Use innerHTML to interpret HTML tags
 
-    // Clear the terminal output after 5 seconds
-    setTimeout(() => {
-        responseText.remove();
-    }, 5000);
+    output.appendChild(responseText);
 }
