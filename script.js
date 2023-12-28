bfbf9ba9dd995055a33ce3949360064556ef3da8
Connor <99085769+TheDevConnor@users.noreply.github.com>
2023-12-28 00:18:26 -0500

    Updated some commands

--- script.js
+++ script.js
@@ -1,29 +1,101 @@
-const terminal=document.querySelector('.terminal');const input=document.getElementById('terminal-input');input.focus();terminal.addEventListener('click',function(){input.focus()});input.addEventListener('keydown',function(event){if(event.key==='Enter'){const command=input.value.trim();const response=executeCommand(command);appendResponse(response);input.value=''}});function checkProject(projectName){switch(projectName){case 'zura':{return `<img src="images/zura.png" alt="Zura Logo" width="100" height="100">
-    Zura is a custom programming language that I created. 
-    It is a dynamically typed language and is a high level language. Which I plane to turn
-    into a low level language. It is currently in development and is 
-    not ready for use. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Zura-v2">here</a>.`}
-case 'chess':{return `Chess is a chess engine that I made using python. It is not that great of a 
-    chess engine, but it is a chess engine. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Basic-Python-Chess-Engine">here</a>.`}
-case 'wordle':{return `Wordle is a word game that I made using html, css, and javascript. It is a game where 
-you have to guess a word in 6 tries. You can find the repository <a target="_blank" href="https://github.com/TheDevConnor/Wordle-Clone">here</a>, and the video <a target="_blank" href="https://www.youtube.com/watch?v=savPJTlsv2k&t=160s">here</a>.`}}}
-function executeCommand(command){if(command==='help'){return'Available commands: help, repo, contact, project, clear'}else if(command==='repo'){return `
-    Repository Information:
-    - Repository Name: Terminal Website
-    - Author: TheDevConnor
-    - Description: A terminal style portfolio website
-    - URL: <a target="_blank" href="https://github.com/TheDevConnor/TerminalWebsite">Link to Repo</a>`}else if(command.startsWith('project')){const projectArgs=command.split(' ');if(projectArgs.length===2){const projectName=projectArgs[1];return `Project Information for ${projectName}: 
-            ${checkProject(projectName)}`}else{return"Please provide a project name from the list: zura, chess, wordle"}}else if(command==='clear'){document.querySelector('.output').innerHTML='';return''}else if(command==='about'){return `
-I am a 18 year old programmer in his first year of collage. I have been programming 
-for 4 years now. I have made many projects in many different languages. I have 
-made a chess engine in python, a wordle clone in html, css, and javascript, and a 
-custom programming language in c++. I am currently working on a custom programming 
-language in c++ called Zura. I also have a youtube channel where I post 
-videos about programming. You can find my channel <a target="_blank" href="https://www.youtube.com/channel/UCpEYiOD5VxkK3iK7JmtbrPQ">here</a>.`}
-else if(command==='contact'){return `
+const terminal = document.querySelector('.terminal');
+const input = document.getElementById('terminal-input');
+input.focus();
+terminal.addEventListener('click', function () {
+  input.focus();
+});
+
+input.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    const command = input.value.trim();
+    const response = executeCommand(command);
+    appendResponse(response);
+    input.value = '';
+  }
+});
+
+function checkProject(projectName) {
+    if (projectName === 'all') {
+        return checkProject('zura') + checkProject('chess') + 
+               checkProject('wordle') + checkProject('terminal');
+    } else if (projectName === 'zura') {
+        return `
+        Project Information:
+        - Project Name: Zura
+        - Author: TheDevConnor
+        - Description: A custom low level programming language written in c++ 
+                       and compiled to x86 assembly
+        - URL: <a target="_blank" href="https://github.com/TheDevConnor/Zura-Transpiled">Link to Repo</a>`;
+    } else if (projectName === 'chess') {
+        return `
+        Project Information:
+        - Project Name: Chess
+        - Author: TheDevConnor
+        - Description: A chess engine written in python
+        - URL: <a target="_blank" href="https://github.com/TheDevConnor/Basic-Python-Chess-Engine">Link to Repo</a>`;
+    } else if (projectName === 'wordle') {
+        return `
+        Project Information:
+        - Project Name: Wordle
+        - Author: TheDevConnor
+        - Description: A wordle clone written in html, css, and javascript
+        - URL: <a target="_blank" href="https://github.com/TheDevConnor/Wordle-Clone">Link to Repo</a>
+        - YouTube Video: <a target="_blank" href="https://www.youtube.com/watch?v=savPJTlsv2k">Link to Video</a>`;
+    } else if (projectName === 'terminal') {
+        return `
+        Project Information:
+        - Project Name: Terminal Website
+        - Author: TheDevConnor
+        - Description: A terminal style portfolio website
+        - URL: <a target="_blank" href="https://github.com/TheDevConnor/TerminalWebsite">Link to Repo</a>
+        - YouTube Video: Comming Soon</a>`;
+    } else {
+        return "Please provide a project name from the list: zura, chess, wordle, or all";
+    }
+}
+
+function executeCommand(command) {
+  if (command === 'help') {
+    return `Available commands: help, contact, project, 
+                    about, youtube, clear`;
+  } else if (command.startsWith('project')) {
+    const projectArgs = command.split(' ');
+    if (projectArgs.length === 2) {
+      const projectName = projectArgs[1];
+      return `Project Information for ${projectName}: 
+            ${checkProject(projectName)}`;
+    } else {
+      return "Please provide a project name from the list: zura, chess, wordle, terminal, or all";
+    }
+  } else if (command === 'clear') {
+    document.querySelector('.output').innerHTML = '';
+    return '';
+  } else if (command === 'about') {
+    return ` Hi, my name is Connor Harris. I have been programming for about 5 years now.
+    I am currently a student at the Indian River State College and plan to go to FIU. 
+    I am currently working on a few projects, including a custom programming language thats main
+    focus is to be easy and simple to use. I am a self taught programmer, and I have learned
+    everything I know from the internet and the people around me. I am always looking to learn
+    more and improve my skills. I have a passion for programming and I hope to one day make a
+    career out of it.`;
+  } else if (command === 'contact') {
+    return `
     Contact Information:
     - Discord: thedevconnor
     - Github: <span><a target="_blank" href="https://github.com/TheDevConnor">Link to Github</a></span>
-    - Email: <a href="mailto:connorharris427@gmail.com"><span class="email">connorharris427@gmail.com</span>`}
-return `Unknown command: ${command}`}
-function appendResponse(response){const output=document.querySelector('.output');const responseText=document.createElement('pre');responseText.innerHTML=response;output.appendChild(responseText)}
+    - Email: <a href="mailto:connorharris427@gmail.com"><span class="email">connorharris427@gmail.com</span></a>`;
+  } else if (command === 'youtube') {
+    return `
+    YouTube Information:
+    - YouTube Channel: TheDevConnor
+    - URL: <a target="_blank" href="https://www.youtube.com/channel/UCpEYiOD5VxkK3iK7JmtbrPQ">Link to Channel</a>`;
+  }
+  return `Unknown command: ${command}`;
+}
+
+function appendResponse(response) {
+  const output = document.querySelector('.output');
+  const responseText = document.createElement('pre');
+  responseText.innerHTML = response;
+  output.appendChild(responseText);
+}
